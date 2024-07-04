e0844ce5a850fbecbd762d141448ba500662f427
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-07-04 15:07:52 +0300

    Add get & delete requests for the restaurant

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -23,6 +23,44 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+@app.route("/restaurants", methods=["GET"])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    restaurants_data = [restaurant.to_dict(only=('address', 'id', 'name')) for restaurant in restaurants]
+    return jsonify(restaurants_data)
+
+@app.route("/restaurants/<int:id>", methods=["GET"])
+def get_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if not restaurant:
+        return jsonify({"error": "Restaurant not found"}), 404
+    
+    restaurant_data = restaurant.to_dict()
+    restaurant_data["restaurant_pizzas"] = []
+    
+    for restaurant_pizza in restaurant.restaurant_pizzas:
+        pizza_data = {
+            "id": restaurant_pizza.id,
+            "pizza_id": restaurant_pizza.pizza_id,
+            "price": restaurant_pizza.price,
+            "restaurant_id": restaurant_pizza.restaurant_id,
+            "pizza": {
+                "id": restaurant_pizza.pizza.id,
+                "name": restaurant_pizza.pizza.name,
+                "ingredients": restaurant_pizza.pizza.ingredients
+            }
+        }
+        restaurant_data["restaurant_pizzas"].append(pizza_data)
+    
+    return jsonify(restaurant_data)
+
+@app.route("/restaurants/<int:id>", methods=["DELETE"])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get_or_404(id)
+    db.session.delete(restaurant)
+    db.session.commit()
+    return '', 204
 
 
 if __name__ == "__main__":
